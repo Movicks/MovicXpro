2a80de264a42f01d83932b28a6524c4b91d163c3
Movicks <codlabx@gmail.com>
2024-04-11 10:22:59 +0100

    new pushes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-       Hello world Victor!
-    </h1>
+    <di className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold underline">
+        Hello Victor !
+      </h1>
+      <p>I am a frontend developer eagle waiting for the right time to fly high</p>
+    </di>
   )
 }
 
