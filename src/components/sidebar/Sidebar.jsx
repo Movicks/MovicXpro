bc0f5a24f5f38168b3348632dff4149b6446d039
Movicks <codlabx@gmail.com>
2024-06-06 18:59:06 +0100

    nav styles

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -26,8 +26,8 @@ const Sidebar = ({ mobileOpen, handleDrawerToggle, isMobile, isTablet }) => {
   }, []);
 
   const drawer = (
-    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }} className='Drawer-container pt-[4rem] flex flex-col gap-1'>
-      <List sx={{ backgroundColor: 'red' }}>
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center', backgroundColor: 'rgba(10, 10, 73, 0.2)' }} className='drawer-container pt-[4rem] flex flex-col gap-2 bg-[rgba(10, 10, 73, 0.2)]'>
+      <List sx={{ backgroundColor: 'transparent' }}>
         {[
           { text: 'Home', path: '/' },
           { text: 'About Me', path: '/about' },
@@ -40,26 +40,32 @@ const Sidebar = ({ mobileOpen, handleDrawerToggle, isMobile, isTablet }) => {
             component={Link}
             to={item.path}
             sx={{
-              backgroundColor: location.pathname === item.path ? 'white' : 'transparent',
+              background: location.pathname === item.path 
+                ? 'linear-gradient(135deg, rgba(88, 99, 246, 0.76), rgba(10, 10, 73, 0.2))' 
+                : 'transparent',
               ml: 0.5,
               borderTopLeftRadius: location.pathname === item.path ? '20px' : 0,
               borderBottomLeftRadius: location.pathname === item.path ? '20px' : 0,
+              pt: 0.2,
+              pb: 0.2,
+              display: 'flex',
+              alignItems: 'center',
             }}
           >
-            <ListItemText primary={item.text} />
+            <ListItemText primary={item.text} className='flex items-center mt-7 mb-7' />
           </ListItemButton>
         ))}
       </List>
       <div className='bg-gray-300 h-[2px]'></div>
-      <section className='Sidebar-Profile pl-2 pr-2'>
+      <section className='sidebar-profile pl-2 pr-2'>
         <div className='glass-effect-2 rounded-2xl overflow-hidden shadow-custom'>
-          <div className='imgContainer pt-9 flex flex-col gap-3'>
+          <div className='img-container pt-9 flex flex-col gap-3'>
             <div>
               <h1 className='text-white font-bold text-[16px]'>Onaiterimoh Victor Idepe</h1>
               <p className='text-gray-200'>Frontend Developer</p>
             </div>
             <section className='h-[17rem] bg-[rgba(10, 10, 73, 0.2)] shadow-custom rounded-2xl relative'>
-              <div className='shadow-custom bg-black-500 border-2 h-full rounded-2xl overflow-hidden'>
+              <div className='shadow-custom border-2 border-[rgba(10, 10, 73, 0.2)] h-full rounded-2xl overflow-hidden'>
                 <img
                   className='w-full h-full bg-cover'
                   src='https://images.unsplash.com/photo-1501196354995-cbb51c65aaea?q=80&w=1742&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
@@ -68,11 +74,21 @@ const Sidebar = ({ mobileOpen, handleDrawerToggle, isMobile, isTablet }) => {
               </div>
             </section>
             <div className='bottom-[-10px] absolute bg-black opacity-70 w-full h-[50px] flex items-center justify-center pb-2 gap-4'>
-              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'><img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/60/0f/0b/600f0bac329e4178179987f4d87fcfab.jpg' /></Link>
-              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'><img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/7f/e9/d7/7fe9d7012076dbda78e984253ee79f49.jpg' /></Link>
-              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'><img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/bb/90/24/bb902442d0880e99732aa1aada0c08de.jpg' /></Link>
-              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'><img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/ef/5c/f1/ef5cf17b90ef75bab569c639b36e52f2.jpg' /></Link>
-              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'><img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/80/2a/19/802a19bdfaec18f714d44db59b456a6e.jpg'/></Link>
+              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'>
+                <img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/60/0f/0b/600f0bac329e4178179987f4d87fcfab.jpg' />
+              </Link>
+              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'>
+                <img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/7f/e9/d7/7fe9d7012076dbda78e984253ee79f49.jpg' />
+              </Link>
+              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'>
+                <img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/cc/fd/22/ccfd2232408dfb656cf784eb8eb625f8.jpg' />
+              </Link>
+              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'>
+                <img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/ef/5c/f1/ef5cf17b90ef75bab569c639b36e52f2.jpg' />
+              </Link>
+              <Link to='#' className='bg-white w-[1.7rem] h-[1.7rem] rounded-[50%] flex items-center justify-center'>
+                <img className='w-[1.3rem] h-[1.3rem] rounded-[50%]' src='https://i.pinimg.com/564x/80/2a/19/802a19bdfaec18f714d44db59b456a6e.jpg' />
+              </Link>
             </div>
           </div>
         </div>      
@@ -81,28 +97,30 @@ const Sidebar = ({ mobileOpen, handleDrawerToggle, isMobile, isTablet }) => {
   );
 
   return (
-    <Box component="nav" className='Nav'>
+    <Box component="nav" className='nav'>
       <Drawer
         variant={(isMobile || isTablet) ? "temporary" : "permanent"}
         open={isMobile || isTablet ? mobileOpen : true}
         onClose={handleDrawerToggle}
         sx={{
           display: { xs: 'block', sm: 'block', md: 'block', lg: 'block' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: 'white' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: '#dfebfa' },
         }}
       >
         {drawer}
       </Drawer>
-      <Drawer
-        variant="permanent"
-        sx={{
-          display: { xs: 'none', sm: 'none', md: 'block', lg: 'block' },
-          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: 'white' },
-        }}
-        open
-      >
-        {drawer}
-      </Drawer>
+      {!isMobile && !isTablet && (
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: 'none', sm: 'none', md: 'block', lg: 'block' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth, backgroundColor: '#dfebfa' },
+          }}
+          open
+        >
+          {drawer}
+        </Drawer>
+      )}
     </Box>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+// const flowbite = require("flowbite-react/tailwind");
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
-
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0px 1px 10px rgba(0, 0, 0, 0.15)',
+      },
+      borderWidth: {
+        '2': '2px',
+      },
+    },
   },
   plugins: [],
 }
